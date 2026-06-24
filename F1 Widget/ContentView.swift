feab14c970dedac0c31b85f9c4a50f260c7db27c
HunterPanda1027 <weihunter05@gmail.com>
2026-06-24 07:30:22 -0400

    Initial Commit

---
+++ F1 Widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  F1 Widget
+//
+//  Created by Hunter Wei on 2026-06-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ F1 Widget/F1_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1_WidgetApp.swift
+//  F1 Widget
+//
+//  Created by Hunter Wei on 2026-06-24.
+//
+
+import SwiftUI
+
+@main
+struct F1_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
